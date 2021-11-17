cd5cce4c260c7782d7de71be3bb646cfed888048
Steven Cybinski <41087154+StevenCyb@users.noreply.github.com>
2021-11-17 18:52:44 +0100

    remove test files

--- build_run.sh
+++
@@ -1,6 +0,0 @@
-# docker stop rtvcc > /dev/null 2>&1
-# docker image rm rtvcc > /dev/null 2>&1
-docker build -t rtvcc .
-docker run --name rtvcc \
-  -v d:/projects/Real-Time-Voice-Cloning-Containerized/volume:/volume rtvcc
-docker rm rtvcc
